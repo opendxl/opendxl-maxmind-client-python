ac0e8d9dd0de73aec0c0781cb8241e080a28e9e6
Jeremy Barlow <jeremy_barlow@mcafee.com>
2017-10-18 09:44:41 -0700

    Various updates for dxlbootstrap 0.1.3 compatibility

--- dist.py
+++ dist.py
@@ -4,31 +4,10 @@ from distutils.dir_util import copy_tree, remove_tree
 from distutils.file_util import copy_file, move_file
 from distutils.core import run_setup
 from distutils.archive_util import make_archive
-from tempfile import mkstemp
-from shutil import move
+
 
 # Run clean
 import clean
-
-
-def replace(file_path, pattern, subst):
-    # Create temp file
-    fh, abs_path = mkstemp()
-    with open(abs_path,'w') as new_file:
-        with open(file_path) as old_file:
-            for line in old_file:
-                new_file.write(line.replace(pattern, subst))
-    os.close(fh)
-    # Remove original file
-    os.remove(file_path)
-    # Move new file
-    move(abs_path, file_path)
-
-
-def remove_dist_files(directory):
-    for f in os.listdir(directory):
-        if f.lower().endswith(".dist"):
-            os.remove(os.path.join(directory, f))
 
 print("Starting dist.\n")
 
@@ -66,8 +45,6 @@ copy_tree(os.path.join(DIST_PY_FILE_LOCATION, "doc", "sdk"), DIST_DOCTMP_DIR)
 print("\nCalling sphinx-build\n")
 subprocess.check_call(["sphinx-build", "-b", "html", DIST_DOCTMP_DIR, os.path.join(DIST_DIRECTORY, "doc")])
 
-replace(os.path.join(DIST_DIRECTORY, "doc", "_static", "classic.css"), "text-align: justify", "text-align: none")
-
 # Delete .doctrees
 remove_tree(os.path.join(os.path.join(DIST_DIRECTORY, "doc"), ".doctrees"), verbose=1)
 # Delete .buildinfo
@@ -103,7 +80,6 @@ run_setup(SETUP_PY,
 
 print("\nCopying sample into dist directory\n")
 copy_tree(os.path.join(DIST_PY_FILE_LOCATION, "sample"), SAMPLE_RELEASE_DIR)
-remove_dist_files(SAMPLE_RELEASE_DIR)
 
 print("\nCopying dist to " + DIST_RELEASE_DIR + "\n")
 copy_tree(DIST_DIRECTORY, DIST_RELEASE_DIR)
